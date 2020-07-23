df5be250b3d1c9d3c6e00c54980411ad1983cad0
Marco Napetti <mnapetti@alpenite.com>
2020-07-23 09:35:06 +0200

    default to SFRAù

--- cartridges/plugin_sf_pwa/cartridge/controllers/Pwa.js
+++ cartridges/plugin_sf_pwa/cartridge/controllers/Pwa.js
@@ -1,33 +1,29 @@
-var guard = require('*/cartridge/scripts/guard');
+var server = require('server');
 
 /**
  * Includes service worker script to current page scope.
  * This is used during Service worker registration.
  */
-function sw() {
-    var template = new dw.util.Template("pwa/sw").render().text;
-    response.setContentType('application/javascript');
-    response.getWriter().print(template);
-}
+server.get('SW', server.middleware.https, function (req, res, next) {
+    res.render("pwa/sw")
+    next();
+});
 
 /**
  * This is called from the head section to include manifest JSON to current page scope.
  */
-function manifest() {
-    var template = new dw.util.Template("pwa/manifest").render().text;
-    response.setContentType('application/manifest+json');
-    response.getWriter().print(template);
-}
+server.get('Manifest', server.middleware.https, function (req, res, next) {
+    res.render("pwa/manifest")
+    next();
+});
 
 /**
  * Renders Generic Offline Page as a fallback in case user is offline 
  * and cache doesn't exist for a particular page.
  */
-function offline() {
-    var ISML = require('dw/template/ISML');
-    ISML.renderTemplate('pwa/offline');
-}
+server.get('Offline', server.middleware.https, function (req, res, next) {
+    res.render('pwa/offline')
+    next();
+});
 
-exports.SW = guard.ensure(['get', 'https'], sw);
-exports.Manifest = guard.ensure(['get', 'https'], manifest);
-exports.Offline = guard.ensure(['get', 'https'], offline);
+module.exports = server.exports();
